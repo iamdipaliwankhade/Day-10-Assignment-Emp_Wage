0b9abb6482224f3dbbf74d2ebf0e575b346563d3
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-12 10:24:16 +0530

    [Dipali] Added : store daily wage along with total wage

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -9,23 +9,40 @@ import java.util.ArrayList;
  */
 public class EmployeeWage implements IEmployeeWage {
 
+	// Declaring a constant
 	public static final int IS_FULLTIME = 1;
 	public static final int IS_PARTTIME = 2;
 
 	private ArrayList<CompanyEmpWage> companyEmpWageList;
+
+	private CompanyEmpWage companyEmpWage;
 
 	public EmployeeWage() {
 		companyEmpWageList = new ArrayList<>();
 	}
 
 	public void addCompany(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
-		CompanyEmpWage companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, totalWorkingDays, totalWorkingHrs);
+		companyEmpWage = new CompanyEmpWage(companyName, wagePerHr, totalWorkingDays, totalWorkingHrs);
 		companyEmpWage.setTotalEmpWage(computeEmployeeWage(wagePerHr, totalWorkingDays, totalWorkingHrs));
 		companyEmpWageList.add(companyEmpWage);
 
 	}
 
-	public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+	public int computeEmployeeWage(int wagePerHr, int totalWorkingDays, int totalWorkingHrs){
+
+	private String companyName;
+	private int wagePerHr;
+	private int totalWorkingDays;
+	private int totalWorkingHrs;
+
+	public EmployeeWage(String companyName, int wagePerHr, int totalWorkingDays, int totalWorkingHrs) {
+		this.companyName = companyName;
+		this.wagePerHr = wagePerHr;
+		this.totalWorkingDays = totalWorkingDays;
+		this.totalWorkingHrs = totalWorkingHrs;
+	}
+
+	public int computeEmployeeWage() {
 		int fullTimeHrs = 8;
 		int partTimeHrs = 4;
 		int totalHrs = 0;
@@ -36,12 +53,32 @@ public class EmployeeWage implements IEmployeeWage {
 			switch (isPresent) {
 			case IS_FULLTIME:
 				totalHrs += fullTimeHrs;
+				companyEmpWage.getDailyWages().add(fullTimeHrs);
 				break;
 			case IS_PARTTIME:
+				totalHrs += partTimeHrs;
+				companyEmpWage.getDailyWages().add(partTimeHrs);
+				break;
+			default:
+				companyEmpWage.getDailyWages().add(0);
+			}
+
+		}
+
+		while (totalHrs < totalWorkingHrs && days < totalWorkingDays) {
+			days++;
+
+			int isPresent = (int) Math.floor(Math.random() * 10) % 3;
+			switch (isPresent) {
+			case 1:
+				totalHrs += fullTimeHrs;
+				break;
+			case 2:
 				totalHrs += partTimeHrs;
 				break;
 			default:
 				totalHrs += 0;
+
 			}
 		}
 		return totalHrs * wagePerHr;
@@ -58,6 +95,8 @@ public class EmployeeWage implements IEmployeeWage {
 
 		for (CompanyEmpWage cmp : employeeWage.companyEmpWageList) {
 			System.out.println(cmp.getCompanyName() + " : " + cmp.getTotalEmpWage());
+			System.out.print(cmp.getDailyWages());
+			System.out.println();
 		}
 
 	}
